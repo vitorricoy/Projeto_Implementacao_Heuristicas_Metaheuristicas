76333eae4d6dba5f363d63180b868c2e62aa9cc3
Vitor Rodarte <vitorricoy@gmail.com>
2022-01-28 12:36:26 -0300

    Resultados parciais dos testes

--- run_tests.py
+++ run_tests.py
@@ -4,21 +4,24 @@ import os
 somaArquivos=0
 cont=0
 for dir in os.listdir("test"):
-    if dir in ['A']:
+    if dir not in ['DIMACS', 'P', 'A', 'E']:
         for file in os.listdir("test/"+dir):
-            if file.endswith(".vrp"):
+            if file.endswith(".vrp") and file == 'B-n39-k5.vrp':
                 arq, ext = os.path.splitext(file)
+                n = int(arq.split('-')[1][1:])
+                if n >= 40:
+                    continue
                 results = []
                 times = []
-                for i in range(5):
-                    command = ["./prog", "test/"+dir+"/"+str(file), "1", "0.75", "100", "5", "10"]
+                for i in range(10):
+                    command = ["./prog", "test/"+dir+"/"+str(file), "1", "0.75", "1000", "5", "10"]
                     start = timer()
                     result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
                     end = timer()
                     results.append(result.stdout)
                     times.append(end-start)
                 print('Escrito', file)
-                with open('test_results/'+str(dir)+'/'+arq+'.sol', 'w') as file:
+                with open('test_results_1000/'+str(dir)+'/'+arq+'.sol', 'w') as file:
                     file.write('Resultados = '+str(results))
                     file.write('\nTempos = '+str(times))
                 
